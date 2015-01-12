37e1900bcade1719cb06a19a4814c6f5fd527f10
Jean-Philippe Boily <j@jipi.ca>
2015-01-12 11:33:23 -0500

    update metrics to be queue_classic 3.1 compatible

--- lib/queue_classic_plus/update_metrics.rb
+++ lib/queue_classic_plus/update_metrics.rb
@@ -2,11 +2,11 @@ module QueueClassicPlus
   module UpdateMetrics
     def self.update
       {
-        "queued" => "queue_classic_jobs",
-        "scheduled" => "queue_classic_later_jobs",
-      }.each do |type, table|
+        "queued" => "=",
+        "scheduled" => "<",
+      }.each do |type, comparison_sign|
         next unless ActiveRecord::Base.connection.table_exists?(table)
-        q = "SELECT q_name, COUNT(1) FROM #{table} GROUP BY q_name"
+        q = "SELECT q_name, COUNT(1) FROM queue_classic_jobs WHERE scheduled_at #{comparison_sign} NOW() GROUP BY q_name"
         results = execute(q)
 
         # Log individual queue sizes
@@ -25,27 +25,25 @@ module QueueClassicPlus
       age = max_age("created_at", "locked_at IS NULL")
       Metrics.measure("qc.max_created_at.unlocked", age)
 
-      if ActiveRecord::Base.connection.table_exists?('queue_classic_later_jobs')
-        lag = execute("SELECT MAX(EXTRACT(EPOCH FROM now() - not_before)) AS lag 
-                FROM queue_classic_later_jobs").first
-                lag = lag ? lag['lag'] : 0
+      lag = execute("SELECT MAX(EXTRACT(EPOCH FROM now() - scheduled_at)) AS lag
+              FROM queue_classic_jobs").first
+              lag = lag ? lag['lag'] : 0
 
-        Metrics.measure("qc.jobs_delayed.lag", lag.to_f)
+      Metrics.measure("qc.jobs_delayed.lag", lag.to_f)
 
-        nb_late = execute("SELECT COUNT(1) 
-           FROM queue_classic_later_jobs 
-           WHERE not_before < NOW()").first
-        nb_late = nb_late ? nb_late['count'] : 0
+      nb_late = execute("SELECT COUNT(1)
+         FROM queue_classic_jobs
+         WHERE scheduled_at < NOW()").first
+      nb_late = nb_late ? nb_late['count'] : 0
 
-        Metrics.measure("qc.jobs_delayed.late_count", nb_late.to_i)
-      end
+      Metrics.measure("qc.jobs_delayed.late_count", nb_late.to_i)
     end
 
     private
     def self.max_age(column, *conditions)
       conditions.unshift "q_name != '#{::QueueClassicPlus::CustomWorker::FailedQueue.name}'"
 
-      q = "SELECT EXTRACT(EPOCH FROM now() - #{column}) AS age_in_seconds 
+      q = "SELECT EXTRACT(EPOCH FROM now() - #{column}) AS age_in_seconds
            FROM queue_classic_jobs
            WHERE #{conditions.join(" AND ")}
            ORDER BY age_in_seconds DESC
