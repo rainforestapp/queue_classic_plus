83c6ee2363ff7d9c291de0ea6b82c2a2c7ed3de5
Mariana Ramos Franco <ramos.franco@gmail.com>
2016-04-14 13:38:37 +0200

    Fixing spec and removing table creation from travis yml

--- spec/queue_classic/queue_spec.rb
+++ spec/queue_classic/queue_spec.rb
@@ -1,13 +1,16 @@
+require 'spec_helper'
+
 describe QC do
 
   describe ".lock" do
 
     context "lock" do
-      QC.enqueue_retry_in(1, "puts", 5, 2)
-      sleep 1
-      job = QC.lock
 
       it "should lock the job with remaining_retries" do
+        QC.enqueue_retry_in(1, "puts", 5, 2)
+        sleep 1
+        job = QC.lock
+
         expect(job[:q_name]).to eq("default")
         expect(job[:method]).to eq("puts")
         expect(job[:args][0]).to be(2)
