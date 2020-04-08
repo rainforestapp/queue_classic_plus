eb039e0bf98c8e4369bb32ae93203bf46bd8384c
Jan Grodowski <jgrodowski@gmail.com>
2020-04-08 16:08:57 +0200

    Port activerecord 6 support from QC::Queue
    original commit (https://github.com/QueueClassic/queue_classic/commit/6ab7dfa4ce658f3b84ef59e7fbe39d6e5586d319) resolves `no implicit conversion of Time into String`.

--- lib/queue_classic_plus/queue_classic/queue.rb
+++ lib/queue_classic_plus/queue_classic/queue.rb
@@ -40,7 +40,7 @@ module QC
             job[:args] = JSON.parse(r["args"])
             job[:remaining_retries] = r["remaining_retries"]
             if r["scheduled_at"]
-              job[:scheduled_at] = Time.parse(r["scheduled_at"])
+              job[:scheduled_at] = r["scheduled_at"].kind_of?(Time) ? r["scheduled_at"] : Time.parse(r["scheduled_at"])
               ttl = Integer((Time.now - job[:scheduled_at]) * 1000)
               QC.measure("time-to-lock=#{ttl}ms source=#{name}")
             end
