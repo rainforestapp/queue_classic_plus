28cdafe1db61ba014fae53d0f94570c163b52c66
Paul Padier <paul.padier@rainforestapp.com>
2023-11-02 10:56:03 +0900

    Handle non-QC+ jobs failing
    When using the ActiveJob QueueClassicAdapter (https://github.com/rails/rails/blob/v7.1.1/activejob/lib/active_job/queue_adapters/queue_classic_adapter.rb), it's possible to enqueue jobs that don't inherit from QueueClassicPlus::Base.
    
    The most common use case of this is ActionMailer::MailDeliveryJobs, hence the previous comment in the code.
    
    When adding argument serializing in 8ae0e2703e081faa8aecd785c1d9fc2f37c5abbb, I didn't check whether the failed job
    class responded to the method, as was done for #disable_retries and #retries_on?
    
    Rather than add yet another respond_to? guard, I opted to simply check once whether the failed job class was a QC+
    descendant, and if not, not bother with the retry logic.

--- lib/queue_classic_plus/worker.rb
+++ lib/queue_classic_plus/worker.rb
@@ -26,15 +26,19 @@ module QueueClassicPlus
 
       @failed_job = job
       @raw_args = job[:args]
-      @failed_job_args = failed_job_class ? failed_job_class.deserialized(@raw_args) : @raw_args
 
-      if force_retry && !(failed_job_class.respond_to?(:disable_retries) && failed_job_class.disable_retries)
-        Metrics.increment("qc.force_retry", source: @q_name)
-        retry_with_remaining(e)
-      # The mailers doesn't have a retries_on?
-      elsif failed_job_class.respond_to?(:retries_on?) && failed_job_class.retries_on?(e)
-        Metrics.increment("qc.retry", source: @q_name)
-        retry_with_remaining(e)
+      if queue_classic_plus_job?
+        @failed_job_args = failed_job_class.deserialized(@raw_args)
+
+        if force_retry && !failed_job_class.disable_retries
+          Metrics.increment("qc.force_retry", source: @q_name)
+          retry_with_remaining(e)
+        elsif failed_job_class.retries_on?(e)
+          Metrics.increment("qc.retry", source: @q_name)
+          retry_with_remaining(e)
+        else
+          enqueue_failed(e)
+        end
       else
         enqueue_failed(e)
       end
@@ -58,6 +62,10 @@ module QueueClassicPlus
 
     def remaining_retries
       (@failed_job[:remaining_retries] || max_retries).to_i
+    end
+
+    def queue_classic_plus_job?
+      failed_job_class && failed_job_class < Base
     end
 
     def failed_job_class
